be5500f547c6cbbafe889d41d3c5b2af9ebbeecf
Marco Cabral <marco@sisne.do>
2017-05-23 09:08:01 -0700

    No create analitic account purchase order

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Analytic Id No Create',
+    'category': 'Purchase',
+    'description':"""
+Do not allow creating analytic accounts on the go. 
+""",
+    'author': 'SisNe, SRL',
+    'website': 'https://sisne.do/',
+    'version': '1.0.1',
+    'depends': ['account'],
+    'data' : [
+        'views/purchase_views.xml',
+    ],
+    'qweb': [],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
